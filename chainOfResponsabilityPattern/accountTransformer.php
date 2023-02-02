7d56e87029d357631bd1d7b7d3f7075472582757
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-02-01 20:42:40 -0600

    Chain Of Responsability for Account validation 1

---
+++ chainOfResponsabilityPattern/accountTransformer.php
@@ -0,0 +1,53 @@
+<?php
+abstract class CRHandler {
+    protected $nextHandler;
+
+    public function setNext(CRHandler $handler){
+        $this->nextHandler = $handler;
+        return $handler;
+    }
+
+    abstract public function handle($account);
+}
+/*
+nodeV.setNext(nodeG).setNext(nodeH).setNext(nodeq);
+*/
+class ReplaceCRHandler extends CRHandler {
+    private int $number;
+    private string $letter;
+    public function __construct(int $number, string $letter)
+    {
+        $this->number = $number;
+        $this->letter = $letter;
+    }
+    public function handle($account){
+        $transformed = str_replace($this->letter, $this->number, $account);
+        if($this->nextHandler){
+            $this->nextHandler->handle($transformed);
+        } else {
+            return $transformed;
+        }
+    }
+}
+class CleanCRHandler extends CRHandler{
+    public function handle($account){
+        $transformed = str_replace([" ", "-"], "", $account);
+        $transformed = strtolower($transformed);
+        if($this->nextHandler){
+            $this->nextHandler->handle($transformed);
+        } else {
+            return $transformed;
+        }
+    }
+}
+class InvertCRHandler extends CRHandler{
+    public function handle($account){
+        $transformed = strrev($account);
+        if($this->nextHandler){
+            $this->nextHandler->handle($transformed);
+        } else {
+            return $transformed;
+        }
+    }
+}
+?>
